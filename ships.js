05111a152638f8019f538b3d8d8cfaa5e14f829b
FatherMundy <mundy@wowsbuilds.com>
2022-10-18 23:13:25 -0400

    Update ships.js

--- ships.js
+++ ships.js
@@ -49,6 +49,8 @@ function flagSelect(x){
     else if (x == "U.K.") {return 'https://uploads-ssl.webflow.com/5f3b00acbb1ebd856f32d560/61a4fbd1cfe6f7704f1d0cbf_uk.svg'}
     else if (x == "U.S.A.") {return 'https://uploads-ssl.webflow.com/5f3b00acbb1ebd856f32d560/61a4fbcdb67f56ed06f0e5f9_usa.svg'}
     else if (x == "U.S.S.R.") {return 'https://uploads-ssl.webflow.com/5f3b00acbb1ebd856f32d560/61a4fbd438320f7a32ca2155_ussr.svg'}
+    else if (x == "Commonwealth") {return 'https://uploads-ssl.webflow.com/5f3b00acbb1ebd856f32d560/6237ce1346d204c08c5113f3_com-flag.svg'}
+  
 }
 
 
